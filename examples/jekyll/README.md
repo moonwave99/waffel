2c065d964138db801b1c3ddb7a55dd9599d43865
moonwave99 <diego.caponera@gmail.com>
2015-04-21 14:55:18 +0200

    Jekyll example draft.

---
+++ examples/jekyll/README.md
@@ -0,0 +1,7 @@
+# Waffel examples - Blog generation (à la Jekyll)
+
+[Jekyll](http://www.jekyll.io) is the static blog generation staple. Let's mimic its behaviour in Waffel!
+
+## Goal
+
+We are going to generate a blog that thoroughly reviewed [the best 1000 movies ever, according to NY Times](http://www.nytimes.com/ref/movies/1000best.html).
---
+++ examples/jekyll/index.js
@@ -0,0 +1,43 @@
+var Waffel = require('Waffel');
+var fs = require('fs-extra');
+var _ = require('lodash');
+var faker = require('faker');
+var yaml = require('yaml-front-matter');
+
+var movies = fs.readFileSync('movies.txt').toString().split("\n");
+var categories = ['classic', 'independent', 'avantgarde'];
+var tags = _.reduce(_.range(25), function(memo, index){
+  memo.push(faker.random.bs_buzz());
+  return memo;
+}, []);
+tags = _.uniq(tags);
+
+movies.forEach(function(movie, index){
+  var data = {
+    title: movie,
+    slug: movie.toLowerCase().replace(/\s+/g, '-').replace(/[^-\w]/g, ''),
+    date: faker.Date.past(3, new Date().toJSON()),
+    category: _.sample(categories),
+    cover: "http://lorempixel.com/g/640/480/" + _.sample(['people', 'city', 'fashion', 'nature', 'nightlife', 'transport']) + '/' + _.random(1, 10),
+    tags: _.sample(tags, _.random(2,5))
+  }
+  
+  fs.outputFileSync(
+    __dirname + '/data/posts/' + data.slug + '.md',
+    "---\n" + yaml.safeDump(data) + "---\n" + _.reduce(_.range(3,10), function(memo, index){
+      memo += faker.Lorem.paragraph(_.random(2,5)) + '\n\n';
+      return memo;
+    }, '')
+  );
+  
+});
+
+var wfl = new Waffel({
+  domain: "http://localhost:8000"
+});
+
+wfl.init().then(function(){
+  wfl.generate().then(function(){
+    console.log('Y0 WAFFEL Y0')
+  })
+})
