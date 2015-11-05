8ab40a0e6811f6bf586a696c16d25667d9795f0f
moonwave99 <diego.caponera@gmail.com>
2015-11-05 20:27:48 +0100

    Promisify generator.

--- docs/examples/jekyll/index.js
+++ docs/examples/jekyll/index.js
@@ -1,20 +1,20 @@
-var Waffel = require('Waffel');
-var generator = require('./lib/generator');
+var Waffel = require('../../../lib/index')
+var generator = require('./lib/generator')
 
 // Generate some data here
-generator();
+generator().then(function(){
+  // We do the Waffel stuff here.
+  var port = 1337
+  var wfl = new Waffel({
+    domain:   "http://localhost:" + port,
+    uglyUrls: true,
+    server:   true,
+    serverConfig: {
+      port:       port,
+      path:       'public',
+      indexPath:  'public/404.html'
+    }
+  })
 
-// We do the Waffel stuff here.
-var port = 1337
-var wfl = new Waffel({
-  domain:   "http://localhost:" + port,
-  uglyUrls: true,
-  server:   true,
-  serverConfig: {
-    port:       port,
-    path:       'public',
-    indexPath:  'public/404.html'
-  }
+  wfl.init().then(function(){ wfl.generate() })
 })
-
-wfl.init().then(function(){ wfl.generate(); })
--- docs/examples/jekyll/lib/generator.js
+++ docs/examples/jekyll/lib/generator.js
@@ -1,10 +1,11 @@
 #! /usr/bin/env node
 
-var fs = require('fs-extra')
 var path = require('path');
 var _ = require('lodash')
 var faker = require('faker')
 var yaml = require('yaml-front-matter')
+var Promise = require('bluebird')
+var fs = Promise.promisifyAll(require('fs-extra'))
 
 module.exports = function(){
   // We fake some data here.
@@ -20,22 +21,23 @@ module.exports = function(){
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
   }
 
-  movies.forEach(function(movie, index){
-    var data = {
-      title: movie,
-      slug: movie.toLowerCase().replace(/\s+/g, '-').replace(/[^-\w]/g, ''),
-      date: randomDate(new Date(2005, 0, 1), new Date()),
-      category: _.sample(categories),
-      cover: "http://lorempixel.com/g/640/480/" + _.sample(['people', 'city', 'fashion', 'nature', 'nightlife', 'transport']) + '/' + _.random(1, 10),
-      tags: _.sample(tags, _.random(2,5))
-    }
-  
-    fs.outputFileSync(
-      path.join(__dirname, '..', 'data/posts/', data.slug + '.md'),
-      "---\n" + yaml.safeDump(data) + "---\n" + _.reduce(_.range(3,10), function(memo, index){
-        memo += faker.Lorem.paragraph(_.random(2,5)) + '\n\n'
-        return memo
-      }, '')
-    )
-  })  
-};
+  return fs.readFileAsync(path.join(__dirname, '..', 'movies.txt'), 'utf8').then(function(data){
+    return Promise.all(data.split("\n").map(function(movie, index){
+      var data = {
+        title: movie,
+        slug: movie.toLowerCase().replace(/\s+/g, '-').replace(/[^-\w]/g, ''),
+        date: randomDate(new Date(2005, 0, 1), new Date()),
+        category: _.sample(categories),
+        cover: "http://lorempixel.com/g/640/480/" + _.sample(['people', 'city', 'fashion', 'nature', 'nightlife', 'transport']) + '/' + _.random(1, 10),
+        tags: _.sample(tags, _.random(2,5))
+      }
+      return fs.outputFileAsync(
+        path.join(__dirname, '..', 'data/posts/', data.slug + '.md'),
+        "---\n" + yaml.safeDump(data) + "---\n" + _.reduce(_.range(3,10), function(memo, index){
+          memo += faker.Lorem.paragraph(_.random(2,5)) + '.\n\n'
+          return memo
+        }, '')
+      )
+    }))
+  })
+}
